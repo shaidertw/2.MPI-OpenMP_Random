66f377037aa66fbcf2814d81fb7832852d351ba7
shaidertw <shaidermeister@gmail.com>
2022-05-29 14:13:23 +0500

    init commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<ctime>
+#include "mpi.h"
+#include "omp.h"
+
+
+int main(int argc, char* argv[]){
+	int numThreads=1;
+	int pRank, total;
+
+	for (int i = 0; i < argc; i++){
+		if (strncmp(argv[i], "--threads", 9) == 0){
+			numThreads = atoi(argv[i + 1]);
+			break;
+		}
+	}
+
+	omp_set_num_threads(numThreads);
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &total);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pRank);
+	//std::cout << "Total processes=" << total << std::endl;
+#pragma omp parallel for private(i)
+	for (int i = 0; i < omp_get_max_threads(); ++i) {
+		int tRank = omp_get_thread_num();
+		srand((i+ 1) * (pRank + 1) ^ time(NULL));
+		int num = rand() % 1000;
+		std::cout << "I am " << i << " thread from " << pRank << " process, my random number = " << num << std::endl;
+	}
+	MPI_Finalize();
+}
+
+
---
+++ mpi.bat
@@ -0,0 +1,2 @@
+@echo on
+"C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -n 5 x64\Release\HelloWorldMPI.exe --threads 3
